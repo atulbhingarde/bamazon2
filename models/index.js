b85ec292f19a16d50db641be3c24e944b5cd58f4
atulbhingarde <atul.bhingarde@ge.com>
2019-01-15 13:53:21 -0500

    added

--- models/index.js
+++ models/index.js
@@ -11,6 +11,7 @@ var db = {};
 if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable]);
   // console.log(" here " + process.env[config.use_env_variable]);
+  console.log('This environment is ' + env);
 } else {
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
