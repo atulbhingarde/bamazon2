44b872d9e87f752f42794f50b324ac47516d2214
atulbhingarde <atul.bhingarde@ge.com>
2019-02-09 18:43:00 -0500

    modified for g

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -2,9 +2,9 @@ const path = require("path");
 
 module.exports = function(app) {
   // If no matching route is found default to index
-  app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
-  });
+  // app.get('/', function(req, res) {
+  //   res.sendFile(path.join(__dirname, '../public/index.html'));
+  // });
 
   
   app.get('*', function(req, res) {
